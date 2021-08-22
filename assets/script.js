3fcbab59ca818124e6529cba6dc371a5018691e0
Arthur Sarmento <arthurbs17@gmail.com>
2021-08-22 20:10:38 -0300

    Oregon trail completo

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+class Traveler {
+    constructor(name) {
+        this.name = name;
+        this.food = 1;
+        this.isHealthy = true;
+    }
+
+    hunt() {
+        this.food += 2;
+    }
+    eat() {
+        if (this.food < 1) {
+            this.isHealthy = false;
+        }
+        if (this.food > 0) {
+            this.isHealthy = true;
+            this.food -= 1;
+        }
+    }
+}
+
+class Wagon {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.passageiros = [];
+    }
+    getAvailableSeatCount() {
+        let availableSeats = this.capacity - this.passageiros.length;
+        if (availableSeats > 0) {
+            return availableSeats;
+        } else {
+            return 0
+        }
+    }
+    join(passageiro) {
+        if (this.getAvailableSeatCount() > 0) {
+            this.passageiros.push(passageiro);
+        }
+    }
+    shouldQuarantine(){
+        for(let i = 0; i < this.passageiros.length; i++){
+            if(this.passageiros[i].isHealthy === false){
+                return true;
+            }
+        }
+        return false
+    }
+    totalFood(){
+        let totalFood = 0;
+        for(let i = 0; i < this.passageiros.length; i++){
+            totalFood += this.passageiros[i].food;
+        }
+        return totalFood;
+    }
+}
